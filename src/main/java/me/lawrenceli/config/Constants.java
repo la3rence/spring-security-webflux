4ad983fdaf2bc6549ce79924aa7a57705ceebaf1
lawrence.li <hi@lawrenceli.me>
2024-09-04 13:39:36 +0800

    feat: refine request id propagation

--- src/main/java/me/lawrenceli/config/Constants.java
+++ src/main/java/me/lawrenceli/config/Constants.java
@@ -3,5 +3,4 @@ package me.lawrenceli.config;
 public class Constants {
     public static final String API_PREFIX = "/api";
     public static final String REQUEST_ID = "X-Request-ID";
-    public static final String REQUEST_CONTEXT = "Request-Context";
 }
--- src/main/java/me/lawrenceli/config/logger/MDCFilter.java
+++ src/main/java/me/lawrenceli/config/logger/MDCFilter.java
@@ -4,7 +4,6 @@ import io.micrometer.context.ContextRegistry;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.slf4j.MDC;
-import org.springframework.http.HttpHeaders;
 import org.springframework.http.server.reactive.ServerHttpRequest;
 import org.springframework.lang.NonNull;
 import org.springframework.stereotype.Component;
@@ -13,9 +12,6 @@ import org.springframework.web.server.WebFilter;
 import org.springframework.web.server.WebFilterChain;
 import reactor.core.publisher.Mono;
 import reactor.util.context.Context;
-
-import java.util.List;
-import java.util.UUID;
 
 import static me.lawrenceli.config.Constants.REQUEST_ID;
 
@@ -26,29 +22,21 @@ public class MDCFilter implements WebFilter {
 
     @Override
     @NonNull
-    public Mono<Void> filter(ServerWebExchange exchange, WebFilterChain chain) {
+    public Mono<Void> filter(ServerWebExchange exchange, @NonNull WebFilterChain chain) {
         ServerHttpRequest request = exchange.getRequest();
-        String requestId = getRequestId(request.getHeaders());
+        String requestId = request.getId();
+
         // https://gist.github.com/chemicL/0e0d8e95e28414f0ecb769a5b8ca326e
-        // maybe we can use https://github.com/Numichi/reactive-logger
         ContextRegistry.getInstance().registerThreadLocalAccessor(REQUEST_ID, () -> MDC.get(REQUEST_ID),
                 value -> MDC.put(REQUEST_ID, requestId), () -> MDC.remove(REQUEST_ID));
-
         MDC.put(REQUEST_ID, requestId);
-        logger.info("Request {}", exchange.getRequest().getPath());
 
-        exchange.getResponse().getHeaders().put(REQUEST_ID, List.of(requestId));
+        exchange.getResponse().getHeaders().set(REQUEST_ID, requestId);
+        logger.info("Request {} {}", request.getMethod(), request.getPath());
+
         return chain.filter(exchange)
                 .contextWrite(Context.of(REQUEST_ID, requestId))
                 .doFinally(s -> MDC.remove(REQUEST_ID));
     }
-
-    private String getRequestId(HttpHeaders headers) {
-        List<String> requestIdHeaders = headers.get(REQUEST_ID);
-        return requestIdHeaders == null || requestIdHeaders.isEmpty()
-                ? UUID.randomUUID().toString()
-                : requestIdHeaders.get(0);
-    }
-
 
 }
